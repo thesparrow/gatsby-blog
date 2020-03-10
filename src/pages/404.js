7901a26a216b89a4cadb3a612319c95e6f32ab39
cafebabe007 <shilovana@gmail.com>
2020-03-10 13:32:14 -0700

    task: adding post features

---
+++ src/pages/404.js
@@ -0,0 +1,13 @@
+import React from "react"
+import Layout from "../components/layout"
+
+
+const Error = () => {
+    return (
+        <Layout>
+            <h1>We hit an error!</h1>
+        </Layout>
+    )
+}
+
+export default Error
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,9 @@
 import React from "react"
+import Layout from "../components/layout"
+import {Link} from 'gatsby' 
 
-export default () => <div>Hello world!!</div>
+export default () => <Layout>Hello world!!
+    <div>   
+        <Link to="/post/"> Post Page</Link>
+    </div>
+</Layout>
