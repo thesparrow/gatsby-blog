cddcdc0351520ec8ab0cd3954a95bf9a86cb6fe3
cafebabe007 <shilovana@gmail.com>
2020-03-10 14:05:04 -0700

    chore: adding component likes.js with css flavor

---
+++ src/components/likes.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const likes = () => {
+    return (
+        <div className="likes">
+            <h3>Click if you like the post.</h3>
+            <h3>likes</h3>
+            <button>click me</button>
+        </div>
+    )
+}
+export default likes 
