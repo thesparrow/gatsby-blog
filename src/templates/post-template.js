82700c8072006ab4c6d39168686bfb4686ce0947
cafebabe007 <shilovana@gmail.com>
2020-03-11 11:21:02 -0700

    task: create query and template for each of our post pages

---
+++ src/templates/post-template.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import styles from '../css/postTemplate.module.css'
+import {Link, graphql} from 'gatsby'
+import Image from 'gatsby-image'
+import Layout from '../components/layout'
+import {MDXRenderer} from 'gatsby-plugin-mdx'
+
+const postTemplate = () => {
+    return (
+        <div>
+            post template
+        </div>
+    )
+}
+
+export default postTemplate
