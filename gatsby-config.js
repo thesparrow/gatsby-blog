169a4bf89cfbe836d48c512421120f34e1962a33
cafebabe007 <shilovana@gmail.com>
2020-03-11 10:39:41 -0700

    task: adding posts with mdx

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,8 +2,16 @@
 module.exports = {
   /* Your site config here */
   plugins: [
-    // ....
+    `gatsby-transformer-sharp`, 
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `posts`,
+        path: `src/posts/`,
+      },
+      
+    },
     `gatsby-plugin-mdx`,
   ],
-
 }
